a1d065a5582fb7506ace49ef26ceebe093ff3280
G`ayratjon Toshpo`latov <107244682+JuniorDev777@users.noreply.github.com>
2022-08-23 12:56:12 +0500

    swich case

---
+++ New folder (2)/code.js
@@ -0,0 +1,19 @@
+
+
+
+I will develop responsive front end website with bootstrap
+US$45
+💡 ESSENTIAL ✅ One page site ✅ Responsive Design ✅ SEO Optimized ✅ User Friendly
+
+
+basik
+
+standart
+US$95
+Save up to 20% with Subscribe to Save
+💡 EXECUTIVE ✅ 3 page site ✅ Responsive Design ✅ SEO Optimized ✅ User Friendly
+
+US$195
+Save up to 20% with Subscribe to Save
+💡 PRO ✅ 5-7 page site ✅ Responsive Design ✅ SEO Optimized ✅ User Friendly
+
